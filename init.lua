078200d7362b3be838d2b8c07bf81bbeffaa7efa
Alexander Vetski <alexander.vetski@contact-software.com>
2024-03-20 21:47:56 +0100

    feat: use leader button for neotree and neogit

--- init.lua
+++ init.lua
@@ -157,8 +157,8 @@ vim.opt.scrolloff = 10
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<C-n>', '<Cmd>Neotree toggle<CR>')
-vim.keymap.set('n', '<C-g>', '<Cmd>Neogit kind=auto<CR>')
+vim.keymap.set('n', '<leader>b', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Neotree' })
+vim.keymap.set('n', '<leader>g', '<Cmd>Neogit kind=auto<CR>', { desc = 'Neogit' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
